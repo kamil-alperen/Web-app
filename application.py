5f08b6df310456c85fe63248e401dbd213ee8014
kamil-alperen <63701451+kamil-alperen@users.noreply.github.com>
2021-09-06 13:47:40 +0300

    no-database

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, redirect, request, url_for, jsonify
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
-from flask_sqlalchemy import SQLAlchemy
+""" from flask_sqlalchemy import SQLAlchemy """
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chess_database.db'
-database = SQLAlchemy(app)
+""" app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chess_database.db'
+database = SQLAlchemy(app) """
 api = Api(app)
 
 
@@ -26,11 +26,11 @@ def exit():
     pass
 
 
-class Table(database.Model):
+""" class Table(database.Model):
     id = database.Column(database.Integer, primary_key = True)
     piece_name = database.Column(database.String(20), nullable = False)
     def __repr__(self):
-        return f"{self.id} => {self.piece_name}"
+        return f"{self.id} => {self.piece_name}" """
 
 
 
@@ -42,11 +42,11 @@ class TableExchange(Resource):
         table = request.json
         if not table:
             abort(404)
-        for list in table:
+        """ for list in table:
             for piece_name_ in list:
                 table_instance = Table(piece_name = piece_name_)
                 database.session.add(table_instance)
-        database.session.commit()
+        database.session.commit() """
         return "OK", 200 
 
     def delete(self):
