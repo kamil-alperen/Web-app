57b113c51f7eea3daac331bc3ceba8708c3230a4
kamil-alperen <63701451+kamil-alperen@users.noreply.github.com>
2021-09-06 13:34:15 +0300

    pop-ups added

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, jsonify
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chess_database.db'
+database = SQLAlchemy(app)
+api = Api(app)
 
 
 @app.route("/")
@@ -11,13 +17,48 @@ def new_game():
     return render_template("chess_new_game.html")
 @app.route("/saved_games")
 def saved_games():
-    return ""
+    return render_template("chess_saved_games.html")
 @app.route("/settings")
 def settings():
-    return ""
+    return render_template("chess_settings.html")
 @app.route("/exit")
 def exit():
-    return ""
+    pass
+
+
+class Table(database.Model):
+    id = database.Column(database.Integer, primary_key = True)
+    piece_name = database.Column(database.String(20), nullable = False)
+    def __repr__(self):
+        return f"{self.id} => {self.piece_name}"
+
+
+
+class TableExchange(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        table = request.json
+        if not table:
+            abort(404)
+        for list in table:
+            for piece_name_ in list:
+                table_instance = Table(piece_name = piece_name_)
+                database.session.add(table_instance)
+        database.session.commit()
+        return "OK", 200 
+
+    def delete(self):
+        pass
+
+    def put(self):
+        pass
+
+
+api.add_resource(TableExchange, '/api/chessgame/table')
+
+
 
 
 if __name__ == "__main__":
--- static/js/chess_new_game.js
+++ static/js/chess_new_game.js
@@ -3,7 +3,12 @@ let clickedSquare = {}
 let suitableElements = []
 let table = []
 let turn = "white"
-
+for(let i = 0;i < 8;i++){
+    num1 = i + 1 + 8
+    num2 = i + 1 + 48
+    squares[num1].moved = false
+    squares[num2].moved = false
+}
 function fillTable(){
     row1 = ["black_castle1","black_horse1","black_elephant1","black_queen1","black_king1","black_elephant2","black_horse2","black_castle2"]
     row8 = ["white_castle1","white_horse1","white_elephant1","white_queen1","white_king1","white_elephant2","white_horse2","white_castle2"]
@@ -30,6 +35,139 @@ function fillTable(){
     }
 }
 fillTable()
+
+
+expandToLeft_element = document.querySelector(".left-expand")
+rightSideBar_element = document.querySelector(".side-bar")
+saveGame_element = document.querySelector(".save-game")
+newGame_element = document.querySelector(".new-game")
+expandToLeft_element.addEventListener("mouseenter", (e) => {
+    el = rightSideBar_element
+    el.style.backgroundColor = "black"
+    el.style.width = "10%"
+    saveGame_element.style.setProperty("display","inline-block")
+    newGame_element.style.setProperty("display","inline-block")
+    expandToLeft_element.style.transform = "rotateY(180deg)"
+})
+rightSideBar_element.addEventListener("mouseleave", (e) => {
+    el = rightSideBar_element
+    el.style.removeProperty('background-color')
+    el.style.setProperty('width','3%')
+    saveGame_element.style.setProperty("display","none")
+    newGame_element.style.setProperty("display","none")
+    expandToLeft_element.style.removeProperty('transform')
+})
+
+finishGame_element = document.querySelector(".finish-game")
+settings_element = document.querySelector(".settings")
+saved_element = document.querySelector(".save-game")
+popUpFinish = document.querySelector(".pop-up-finish-game")
+popUpSettings = document.querySelector(".pop-up-settings")
+popUpSaved = document.querySelector(".pop-up-saved")
+popUpTop = document.querySelector(".pop-up-top")
+clearBtn = document.querySelector("#clearbtn")
+submitBtn = document.querySelector("#submitbtn")
+field1 = document.querySelector("#player1")
+field2 = document.querySelector("#player2")
+field3 = document.querySelector("#gamename")
+field4 = document.querySelector("#descr")
+popUpSuccess = document.querySelector('.pop-up-success')
+
+clearBtn.addEventListener('click', (e) => {
+    field1.value = ''
+    field2.value = ''
+    field3.value = ''
+    field4.value = ''
+})
+submitBtn.addEventListener('click',(e) => {
+    if(field3.value === ''){
+        field3.style.setProperty('border','5px solid red')
+    }
+    else {
+        fetch('/api/chessgame/table',{
+            method : 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body : JSON.stringify(table)
+        }).then(response => response.json())
+        .then(data => console.log(data))
+        popUpSaved.style.display = 'none'
+        popUpSuccess.textContent = 'Game is saved successfully'
+        popUpSuccess.style.display = 'block'
+        e.stopPropagation()
+    }
+})
+settings_element.addEventListener('click', (e) => {
+    popUpSettings.style.display = 'block'
+    popUpTop.style.setProperty('text-align','center')
+    popUpTop.textContent = 'Settings'
+    popUpTop.style.setProperty('padding-top','0.2em')
+    popUpSettings.appendChild(popUpTop)
+    e.stopPropagation()
+})
+
+saved_element.addEventListener('click', (e) => {
+    popUpSaved.style.display = 'block'
+    popUpTop.style.setProperty('text-align','center')
+    popUpTop.textContent = 'Save Game'
+    popUpTop.style.setProperty('padding-top','0.2em')
+    popUpSaved.appendChild(popUpTop)
+    e.stopPropagation()
+})
+
+finishGame_element.addEventListener("click", (e) => {
+    black_points = 0
+    white_points = 0
+    for(let i = 0;i < table.length;i++){
+        for(let j = 0;j < table[0].length;j++){
+            piece = table[i][j]
+            color = piece.substring(0, piece.indexOf("_"))
+            namePiece = piece.substring(piece.indexOf("_")+1, piece.length-1)
+            if(color === "black"){
+                switch(namePiece){
+                    case "pawn":
+                        black_points+=1
+                        break
+                    case "queen":
+                        black_points+=5
+                        break
+                    case "king":
+                        black_points+=7
+                        break
+                    default:
+                        black_points+=3
+                }
+            }   
+            else if(color === "white"){
+                switch(namePiece){
+                    case "pawn":
+                        white_points+=1
+                        break
+                    case "queen":
+                        white_points+=5
+                        break
+                    case "king":
+                        white_points+=7
+                        break
+                    default:
+                        white_points+=3
+                }
+            }   
+        }
+    }
+    winner = black_points === white_points ? "Draw" : black_points > white_points ? "Black" : "White"
+    popUpFinish.style.display = "block"
+    
+    popUpFinish.innerHTML = `Black Points : ${black_points}/38 <br>White Points : ${white_points}/38 <br>Winner : ${winner}`;
+    popUpFinish.style.setProperty('text-align','center')
+    popUpTop.textContent = 'Score Table'
+    popUpTop.style.setProperty('text-align','center')
+    popUpTop.style.setProperty('padding-top','0.2em')
+    popUpFinish.appendChild(popUpTop);
+    e.stopPropagation()
+})
+
 document.getElementById("html").onclick = (event) => {
     tableEl = document.querySelector(".table")
     targetParent = event.target.parentElement
@@ -39,10 +177,31 @@ document.getElementById("html").onclick = (event) => {
             restoreSuitableElements()
         }
     }
+    par_par = event.target.parentElement.parentElement
+    par_par_par = par_par.parentElement
+    if(event.target !== popUpFinish && event.target !== popUpTop){
+        popUpFinish.style.display = "none"
+    }
+    if(par_par !== popUpSettings && event.target !== popUpTop && par_par_par !== popUpSettings){
+        popUpSettings.style.display = "none"
+    }
+    if(par_par !== popUpSaved && event.target !== popUpTop && par_par_par !== popUpSaved){
+        popUpSaved.style.display = "none"
+    }
+    if(event.target !== popUpSuccess){
+        popUpSuccess.style.display = "none"
+        if(popUpSaved.style.display === 'none'){
+            field3.style.setProperty('border','')
+            field1.value = ''
+            field2.value = ''
+            field3.value = ''
+            field4.value = ''
+        }
+    }
 }
 
 for(let i = 0;i < squares.length;i++){
-    squares[i].onclick = () => {
+    squares[i].addEventListener("click", squares[i].firstClickEvent = () => {
         if(checkTurn(findPiece(squares[i]))){ 
             if(clickedSquare.element){
                 clickedSquare.element.style.backgroundColor = clickedSquare.originalBackgroundColor
@@ -57,9 +216,8 @@ for(let i = 0;i < squares.length;i++){
             }
             findAllCapabilities(squares[i])
         }
-    }
+    })
 }
-
 
 function findAllCapabilities(squareElement){
     restoreSuitableElements()
@@ -97,11 +255,12 @@ function findAllCapabilities(squareElement){
 }
 function movePiece(){
     for(let i = 0;i < suitableElements.length;i++){
-        suitableElements[i].element.onclick = function(){
-            /*  getProperty, removeProperty, setProperty için css rule kullanılır */
-            /* getAttribute, setAttribute, removeAttribute için style rule kullanılır */
-            /* element'in property ve attribute'u ile style'ının propert, attribute'u farklı şeylerdir */
-            /* document.styleSheets[0].cssRules[5].style.removeProperty("background-color") */
+        suitableElements[i].element.removeEventListener("click", suitableElements[i].element.firstClickEvent);
+        suitableElements[i].element.addEventListener("click", suitableElements[i].element.secondClickEvent = () => {
+            /*  getProperty, removeProperty, setProperty style için geçerlidir ve css rule kullanılır => sadece inline'daki style'a etki eder */
+            /* getAttribute, setAttribute, removeAttribute element attribute'ları için geçerlidir */
+            /* document.styleSheets[i].cssRules[j].style.removeProperty("background-color")*/
+            suitableElements[i].element.moved = true
             clickedSquare.element.style.backgroundColor = clickedSquare.originalBackgroundColor
             suitableElements[i].element.style.backgroundImage = getComputedStyle(clickedSquare.element).backgroundImage
             clickedSquare.element.style.backgroundImage = "url('')"
@@ -110,8 +269,10 @@ function movePiece(){
             turn = turn === "white" ? "black" : "white"
             isGameOver()
             
-        }
+            
+        });
     }
+    
 }
 function isGameOver(){
     white_king = "white_king1"
@@ -139,20 +300,10 @@ function isGameOver(){
     }
 }
 function restoreOnclickEvents(){
-    for(let i = 0;i < squares.length;i++){
-        squares[i].onclick = () => {    
-            if(checkTurn(findPiece(squares[i]))){
-                if(clickedSquare.element){
-                    clickedSquare.element.style.backgroundColor = clickedSquare.originalBackgroundColor
-                }
-                clickedSquare = {
-                    element : squares[i],
-                    originalBackgroundColor : squares[i].originalBackgroundColor
-                }
-                squares[i].style.backgroundColor = "yellow"
-                findAllCapabilities(squares[i])
-            }
-        }
+    for(let object of suitableElements){
+        object.element.removeEventListener("click", object.element.secondClickEvent);
+        object.element.addEventListener("click", object.element.firstClickEvent);
+        
     }
 }
 function restoreTable(index){
@@ -169,12 +320,13 @@ function restoreTable(index){
 
 }
 function restoreSuitableElements(){
+    restoreOnclickEvents()
     for(let i = 0;i < suitableElements.length;i++){
         oldSuitableElement = suitableElements[i].element
         oldSuitableElement.style.backgroundColor = suitableElements[i].oldBackgroundColor
     }
     suitableElements = []
-    restoreOnclickEvents()
+    
 }
 function findPiece(squareElement){
     className = squareElement.className
@@ -188,7 +340,15 @@ function findPiece(squareElement){
     return [color, name, position_x, position_y]
     
 }
-
+function findElement(position_x, position_y){
+    let squareNumber = position_x + (position_y-1) * 8
+    if(squares[squareNumber].moved == false){
+        return true
+    }
+    else {
+        return false
+    }
+}
 function determineMoveFunction(color, name){
     name = name.substring(0, name.length-1)
     if(name === "castle"){
@@ -471,7 +631,7 @@ function determineMoveFunction(color, name){
                     pieceColor = pieceStr.substring(0,pieceStr.indexOf("_"))
                     if(pieceColor === ""){
                         allMoves.push([position_x,position_y+1])
-                        if(position_y+2 <= 8){
+                        if(position_y+2 <= 8 && findElement(position_x, position_y)){
                             pieceStr = table[position_y+1][position_x-1]
                             pieceColor = pieceStr.substring(0,pieceStr.indexOf("_"))
                             if(pieceColor === ""){
@@ -505,7 +665,7 @@ function determineMoveFunction(color, name){
                     pieceColor = pieceStr.substring(0,pieceStr.indexOf("_"))
                     if(pieceColor === ""){
                         allMoves.push([position_x,position_y-1])
-                        if(position_y-2 >= 1){
+                        if(position_y-2 >= 1 && findElement(position_x, position_y)){
                             pieceStr = table[position_y-3][position_x-1]
                             pieceColor = pieceStr.substring(0,pieceStr.indexOf("_"))
                             if(pieceColor === ""){
